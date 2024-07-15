1de27866c92d168f192d216de8cc862a8175fe66
aldreks1 <cloneartem@mail.ru>
2024-07-15 17:31:57 +0300

    layout updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import CurrencyConverter from "./components/CurrencyConverter";
-
 const App: React.FC = () => {
   return (
     <Provider store={store}>
